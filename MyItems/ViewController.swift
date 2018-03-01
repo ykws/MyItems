be7fb4275e61b66d2a2892349472330b0d4cdf57
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2018-03-01 21:40:27 +0900

    Rewrite long press base

--- MyItems/ViewController.swift
+++ MyItems/ViewController.swift
@@ -8,51 +8,109 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
-  // MARK: - Properties
+  var add = UILabel()
+  var longPressRecognizer: UILongPressGestureRecognizer!
+  //+ボタン
+  @IBAction func addlabel(_ sender: Any) {
   
-  let initialItems: [String] = [
-    "C",
-    "C#",
-    "C++",
-    "Java",
-    "JavaScript",
-    "Perl",
-    "PHP",
-    "Python",
-    "Objective-C",
-    "Ruby",
-    "Rust",
-    "Swift"
-  ]
-  
-  var items: [Item] = Array()
-  
-  // MARK: - Actions
-  
-  @IBAction func addItem(_ sender: Any) {
-    let alert = UIAlertController(title: "Add item", message: "Enter item's name", preferredStyle: .alert)
-    alert.addTextField(configurationHandler: nil)
-    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action:UIAlertAction) -> Void in
-      if let textFields = alert.textFields {
-        for textField in textFields {
-          self.items.append(Item(name: textField.text!))
-        }
-        self.tableView.reloadData()
-      }
-    }))
-    present(alert, animated: true, completion: nil)
+    alert()
+    //label()
   }
   
-  // MARK: - Life Cycle
+  //ラベルについて
+  func label(){
+    add = UILabel(frame: CGRect(x: 130, y:250, width: 100, height:20))
+    //ラベルの大きさ、座標指定
+    //add.text = "labelです"
+    //文字を変更
+    
+    add.backgroundColor = UIColor.lightGray
+    
+    add.font = UIFont.systemFont(ofSize: 30)
+    //文字の大きさ
+    
+    add.textColor = UIColor.black
+    //文字カラー
+    
+    add.sizeToFit()
+    //文字数にあわせてlabelの大きさを変更(サイズが文字にフィットする)
   
+    self.view.addSubview(add)
+    //実際にviewに見える形でlabelが出現する
+  }
+  
+  func alert(){
+    // テキストフィールド付きアラート表示
+    
+    let alert = UIAlertController(title: "タイトル", message: "メッセージ", preferredStyle: .alert)
+    
+    // OKボタンの設定
+    let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+      (action:UIAlertAction!) -> Void in
+      
+      // OKを押した時入力されていたテキストを表示
+      if let textFields = alert.textFields {
+        
+        // アラートに含まれるすべてのテキストフィールドを調べる
+        for textField in textFields {
+          self.label()
+          self.add.text = textField.text!
+          self.add.sizeToFit()
+          print(textField.text!)
+          self.longPressGesture()
+        }
+      }
+    })
+    alert.addAction(okAction)
+    
+    // キャンセルボタンの設定
+    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+    alert.addAction(cancelAction)
+    
+    // テキストフィールドを追加
+    alert.addTextField(configurationHandler: {(textField: UITextField!) -> Void in
+      textField.placeholder = "テキスト"
+    })
+    
+    alert.view.setNeedsLayout() // シミュレータの種類によっては、これがないと警告が発生
+    
+    // アラートを画面に表示
+    self.present(alert, animated: true, completion: nil)
+    
+  }
+  
+  func longpress(sender: UILongPressGestureRecognizer){
+  
+    // 長押し開始〜
+    if(sender.state == UIGestureRecognizerState.began)
+    {
+      
+    } else if (sender.state == UIGestureRecognizerState.ended)
+    {
+      alert() //長押しタップによってアラートを呼び出しlabelの内容を変更したいです。
+      print("ロングタップされたよ。")
+    }
+    
+  }
+
+  func longPressGesture() {
+  
+    // UILongPressGestureRecognizerインスタンス作成
+    let longPressGesture = UILongPressGestureRecognizer(target: self, action: Selector(("longPressGesture")))
+    //デリゲート先を自分に設定する。
+    //longPressRecognizer.delegate
+    // 時間（デフォルト0.5秒）
+    longPressGesture.minimumPressDuration = 0.5
+    self.add.isUserInteractionEnabled = true
+    self.add.addGestureRecognizer(longPressGesture)
+    
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    for name in initialItems {
-      items.append(Item(name: name))
-    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -60,28 +118,4 @@ class ViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
 
-  // MARK: - TableView
-  
-  override func numberOfSections(in tableView: UITableView) -> Int {
-    return 1
-  }
-
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return items.count
-  }
-  
-  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    let item = items[indexPath.row]
-    cell.textLabel?.text = item.name
-    cell.accessoryType = item.complete ? .checkmark : .none
-    return cell
-  }
-  
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    items[indexPath.row].toggle()
-    tableView.deselectRow(at: indexPath, animated: true)
-    tableView.reloadData()
-  }
 }
-
